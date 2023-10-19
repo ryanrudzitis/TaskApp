ba1c531a0db65a33b09903580b4d52f5664d3b71
ryanrudzitis <ryanrudzitis@icloud.com>
2023-10-19 10:19:43 -0700

    initial commit

---
+++ Documents - Ryan’s M1 MacBook Air/BCIT_CST/Term_3/3717/lab7_ryanrudzitis/app/src/main/res/QuestAdapter.kt
@@ -0,0 +1,39 @@
+package 
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class QuestAdapter(private val mList: List<String>) : RecyclerView.Adapter<QuestAdapter.ViewHolder>() {
+
+    // Holds the views
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        //val textView: TextView = itemView.findViewById(R.id.textView) 
+    }
+
+    // create new views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_quests, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        
+        //holder.textView.text = mList[position]
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+}
